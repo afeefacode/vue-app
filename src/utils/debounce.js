49bec2ec8aee732efd53eeb370559cc8497ac064
Kaken Bok <bok@kakenbok.de>
2021-06-15 10:29:29 +0200

    skip debounce on condition

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,11 +1,17 @@
-export function debounce (callback, delay = 300) {
+export function debounce (callback, delay = 300, condition = () => true) {
   let timeout = null
   return function () {
+    const args = arguments
+
     if (timeout) {
       clearTimeout(timeout)
     }
 
-    const args = arguments
+    if (!condition(...args)) {
+      callback(...args)
+      return
+    }
+
     timeout = setTimeout(() => {
       callback(...args)
     }, delay)
