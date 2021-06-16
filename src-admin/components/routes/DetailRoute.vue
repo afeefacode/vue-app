682c40496fce0b92af51737762358a08626bc109
Kaken Bok <bok@kakenbok.de>
2021-06-16 17:56:04 +0200

    add delete button to detail page

--- src-admin/components/routes/DetailRoute.vue
+++ src-admin/components/routes/DetailRoute.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <router-link
-      class="button"
-      :to="model.getLink('edit')"
-    >
-      <v-btn>Bearbeiten</v-btn>
-    </router-link>
+    <v-row class="buttons">
+      <router-link
+        class="button"
+        :to="model.getLink('edit')"
+      >
+        <v-btn>Bearbeiten</v-btn>
+      </router-link>
+
+      <v-btn
+        color="red"
+        class="white--text"
+        @click="remove"
+      >
+        Löschen
+      </v-btn>
+    </v-row>
 
     <component
       :is="Component"
@@ -16,6 +26,8 @@
 
 <script>
 import { Component, Vue } from 'vue-property-decorator'
+import { AlertEvent, DialogEvent, SaveEvent } from '@a-vue/events'
+import { sleep } from '@a-vue/utils/timeout'
 
 @Component({
   props: ['model']
@@ -24,11 +36,53 @@ export default class DetailRoute extends Vue {
   get Component () {
     return this.$routeDefinition.config.routing.detail.Component
   }
+
+  get deleteAction () {
+    const config = this.$routeDefinition.config.routing.delete
+    return config.action
+  }
+
+  async remove () {
+    const result = await this.$events.dispatch(new DialogEvent(DialogEvent.SHOW, {
+      title: this.model.getTitle() + ' löschen?',
+      message: 'Soll ' + this.model.getTitle() + 'gelöscht werden?',
+      yesButton: 'Ja, Löschen!'
+    }))
+
+    if (result === DialogEvent.RESULT_YES) {
+      this.$events.dispatch(new SaveEvent(SaveEvent.START_SAVING))
+
+      const result = await this.deleteAction.request()
+        .params({
+          id: this.model.id
+        })
+        .send()
+
+      await sleep()
+
+      this.$events.dispatch(new SaveEvent(SaveEvent.STOP_SAVING))
+
+      if (result) {
+        this.$events.dispatch(new AlertEvent(AlertEvent.MESSAGE, {
+          message: 'Die Daten wurden gelöscht.'
+        }))
+        this.$router.push(this.model.getLink('list'))
+      } else {
+        this.$events.dispatch(new AlertEvent(AlertEvent.ERROR, {
+          message: 'Die Daten wurden nicht gelöscht.'
+        }))
+      }
+    }
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
+.buttons {
+  gap: 1rem;
+}
+
 .button {
   display: block;
   margin-bottom: 2rem;
