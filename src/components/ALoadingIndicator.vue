a2e623770c8662955044e66e10513c9f6bc4f4c9
Kaken Bok <bok@kakenbok.de>
2021-06-21 21:15:04 +0200

    improve loading indicator duration

--- src/components/ALoadingIndicator.vue
+++ src/components/ALoadingIndicator.vue
@@ -16,6 +16,7 @@ import { Component, Vue, Watch } from 'vue-property-decorator'
 export default class ALoadingIndicator extends Vue {
   internalLoading = false
   timeout = null
+  start = 0
 
   created () {
     this.internalLoading = this.isLoading
@@ -24,13 +25,18 @@ export default class ALoadingIndicator extends Vue {
   @Watch('isLoading')
   isLoadingChanged () {
     if (this.isLoading) { // start immediately, but stop delayed
+      this.start = Date.now()
       this.internalLoading = this.isLoading
       clearTimeout(this.timeout)
     }
 
-    this.timeout = setTimeout(() => {
-      this.internalLoading = false
-    }, 250)
+    if (!this.isLoading) {
+      const diff = Date.now() - this.start
+      const rest = Math.max(0, 250 - diff)
+      this.timeout = setTimeout(() => {
+        this.internalLoading = false
+      }, rest)
+    }
   }
 }
 </script>
