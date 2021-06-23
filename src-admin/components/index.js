c944021af475896a3848dbcb64d64663dee271a5
Kaken Bok <bok@kakenbok.de>
2021-06-23 17:38:18 +0200

    remove lot of obsolete components

--- src-admin/components/index.js
+++ src-admin/components/index.js
@@ -11,15 +11,10 @@ import ListTitle from './list/ListTitle'
 import ListView from './list/ListView'
 import ModelCount from './model/ModelCount'
 import ModelIcon from './model/ModelIcon'
-import ModelView from './model/ModelView'
 import CreatePage from './routes/CreatePage'
-import CreateRoute from './routes/CreateRoute'
 import DetailPage from './routes/DetailPage'
-import DetailRoute from './routes/DetailRoute'
 import EditPage from './routes/EditPage'
-import EditRoute from './routes/EditRoute'
 import ListPage from './routes/ListPage'
-import ListRoute from './routes/ListRoute'
 import Splash from './Splash'
 
 Vue.component('ListCard', ListCard)
@@ -28,19 +23,14 @@ Vue.component('ListMeta', ListMeta)
 Vue.component('ListTitle', ListTitle)
 Vue.component('ListView', ListView)
 Vue.component('ListPage', ListPage)
-Vue.component('ListRoute', ListRoute)
 
 Vue.component('CreatePage', CreatePage)
-Vue.component('CreateRoute', CreateRoute)
 
-Vue.component('EditRoute', EditRoute)
 Vue.component('EditPage', EditPage)
 
 Vue.component('ModelCount', ModelCount)
 Vue.component('ModelIcon', ModelIcon)
-Vue.component('ModelView', ModelView)
 
-Vue.component('DetailRoute', DetailRoute)
 Vue.component('DetailContent', DetailContent)
 Vue.component('DetailMeta', DetailMeta)
 Vue.component('DetailTitle', DetailTitle)
