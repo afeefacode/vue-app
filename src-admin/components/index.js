3373dedb7dd5d7b5ba3153e81ebce9510be0b43f
Kaken Bok <bok@kakenbok.de>
2021-06-23 14:44:13 +0200

    add new routing and page components

--- src-admin/components/index.js
+++ src-admin/components/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import DetailContent from './detail/DetailContent'
 import DetailMeta from './detail/DetailMeta'
 import DetailTitle from './detail/DetailTitle'
+import Index from './Index'
 import ListCard from './list/ListCard'
 import ListContent from './list/ListContent'
 import ListMeta from './list/ListMeta'
@@ -11,9 +12,13 @@ import ListView from './list/ListView'
 import ModelCount from './model/ModelCount'
 import ModelIcon from './model/ModelIcon'
 import ModelView from './model/ModelView'
+import CreatePage from './routes/CreatePage'
 import CreateRoute from './routes/CreateRoute'
+import DetailPage from './routes/DetailPage'
 import DetailRoute from './routes/DetailRoute'
+import EditPage from './routes/EditPage'
 import EditRoute from './routes/EditRoute'
+import ListPage from './routes/ListPage'
 import ListRoute from './routes/ListRoute'
 import Splash from './Splash'
 
@@ -22,10 +27,14 @@ Vue.component('ListContent', ListContent)
 Vue.component('ListMeta', ListMeta)
 Vue.component('ListTitle', ListTitle)
 Vue.component('ListView', ListView)
+Vue.component('ListPage', ListPage)
 Vue.component('ListRoute', ListRoute)
 
+Vue.component('CreatePage', CreatePage)
 Vue.component('CreateRoute', CreateRoute)
+
 Vue.component('EditRoute', EditRoute)
+Vue.component('EditPage', EditPage)
 
 Vue.component('ModelCount', ModelCount)
 Vue.component('ModelIcon', ModelIcon)
@@ -35,6 +44,7 @@ Vue.component('DetailRoute', DetailRoute)
 Vue.component('DetailContent', DetailContent)
 Vue.component('DetailMeta', DetailMeta)
 Vue.component('DetailTitle', DetailTitle)
+Vue.component('DetailPage', DetailPage)
 
 Vue.component('Index', Index)
 Vue.component('Splash', Splash)
