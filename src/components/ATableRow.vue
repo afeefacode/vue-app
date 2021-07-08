ace677aa11bd914179246210045dcefa3380b057
Kaken Bok <bok@kakenbok.de>
2021-07-08 14:07:04 +0200

    table-row.selected

--- src/components/ATableRow.vue
+++ src/components/ATableRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['a-table-row', getFontSizeCssClass, {border: hasBorder}]">
+  <div :class="['a-table-row', getFontSizeCssClass, {border: hasBorder, selected}]">
     <slot />
   </div>
 </template>
@@ -9,7 +9,7 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-  props: ['small']
+  props: ['small', 'selected']
 })
 export default class ATableRow extends Vue {
   $hasOptions = ['border']
@@ -51,14 +51,14 @@ export default class ATableRow extends Vue {
   &.border {
     border-bottom: 1px solid #E5E5E5;
     > * {
-      padding: .4rem;
+      padding: .4rem 1.5rem .4rem .4rem;
       &:last-child {
         padding-right: .4rem;
       }
     }
   }
 
-  &:hover {
+  &:hover, &.selected {
     background: #F4F4F4;
   }
 
@@ -68,6 +68,13 @@ export default class ATableRow extends Vue {
 
   &.small {
     font-size: .9rem;
+
+    &.border {
+      > * {
+        padding-top: .2rem;
+        padding-bottom: .2rem;
+      }
+    }
   }
 }
 </style>
