715495ad26a6a4db9af2de05d56bd4e2e4a8f8ce
Kaken Bok <bok@kakenbok.de>
2021-07-21 21:16:14 +0200

    scroll to top on route change

--- src/plugins/route-config/RouteConfigPlugin.js
+++ src/plugins/route-config/RouteConfigPlugin.js
@@ -147,6 +147,13 @@ class RouteConfigPlugin {
 
         const options = {
           mode: 'history',
+          scrollBehavior (_to, _from, savedPosition) {
+            if (savedPosition) {
+              return savedPosition
+            } else {
+              return { x: 0, y: 0 }
+            }
+          },
           ...this._routerOptions
         }
 
