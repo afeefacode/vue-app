65187018991e56cf1ffe2a8f38e9aae495cb26a2
Kaken Bok <bok@kakenbok.de>
2022-03-04 14:43:27 +0100

    basic not found component

---
+++ src-admin/components/NotFound.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="notFound">
+    <a-row
+      vertical
+      center
+      class="content"
+    >
+      <div class="logo">
+        <slot />
+      </div>
+
+      <a-row
+        vertical
+        center
+      >
+        <div class="mt-0">
+          {{ title || '404 - Not Found' }}
+        </div>
+
+        <div class="mt-4">
+          <router-link :to="backlink || { path: '/'}">
+            {{ backlinkTitle || 'Zurück' }}
+          </router-link>
+        </div>
+      </a-row>
+    </a-row>
+  </div>
+</template>
+
+
+<script>
+import { Component, Vue } from '@a-vue'
+
+@Component({
+  name: 'adminNotFound',
+  props: ['title', 'backlink', 'backlinkTitle']
+})
+export default class NotFound extends Vue {
+  created () {
+    this.$parent.$emit('appLoaded')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.notFound {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  min-height: 50vh;
+  transform: translate(-50%, -50%);
+}
+
+.logo {
+  margin-bottom: 2rem;
+}
+</style>
