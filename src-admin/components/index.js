671021ec02727e78386c9aa51020b2af66aa4abf
Kaken Bok <bok@kakenbok.de>
2022-05-31 20:54:09 +0200

    remove generic page and route components (create, detail, edit, list) from admin

--- src-admin/components/index.js
+++ src-admin/components/index.js
@@ -17,10 +17,7 @@ import ListTitle from './list/ListTitle'
 import ListView from './list/ListView'
 import ModelCount from './model/ModelCount'
 import ModelIcon from './model/ModelIcon'
-import CreatePage from './pages/CreatePage'
-import DetailPage from './pages/DetailPage'
 import EditPage from './pages/EditPage'
-import ListPage from './pages/ListPage'
 import Start from './Start.vue'
 
 Vue.component('ListCard', ListCard)
@@ -29,9 +26,6 @@ Vue.component('ListContent', ListContent)
 Vue.component('ListMeta', ListMeta)
 Vue.component('ListTitle', ListTitle)
 Vue.component('ListView', ListView)
-Vue.component('ListPage', ListPage)
-
-Vue.component('CreatePage', CreatePage)
 
 Vue.component('EditPage', EditPage)
 Vue.component('EditFormButtons', EditFormButtons)
@@ -43,7 +37,6 @@ Vue.component('ModelIcon', ModelIcon)
 Vue.component('DetailContent', DetailContent)
 Vue.component('DetailMeta', DetailMeta)
 Vue.component('DetailTitle', DetailTitle)
-Vue.component('DetailPage', DetailPage)
 Vue.component('DetailProperty', DetailProperty)
 Vue.component('DetailColumn', DetailColumn)
 
--- src-admin/config/routing.js
+++ src-admin/config/routing.js
@@ -1,19 +1,8 @@
-import CreateRoute from '@a-admin/components/routes/CreateRoute'
-import DetailRoute from '@a-admin/components/routes/DetailRoute'
-import EditRoute from '@a-admin/components/routes/EditRoute'
-import ListRoute from '@a-admin/components/routes/ListRoute'
 import { routeConfigPlugin } from '@a-vue/plugins/route-config/RouteConfigPlugin'
 
 export default routeConfigPlugin
   .router({
     linkActiveClass: 'active'
-  })
-
-  .defaultComponents({
-    list: ListRoute,
-    new: CreateRoute,
-    detail: DetailRoute,
-    edit: EditRoute
   })
 
   .defaultBreadcrumbTitles({
