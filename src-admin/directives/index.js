91bcb2ecd79c0b933f5beaac140fa6c778447dcf
Kaken Bok <bok@kakenbok.de>
2022-06-02 10:04:42 +0200

    ensure flying-context-trigger directive css class persists

--- src-admin/directives/index.js
+++ src-admin/directives/index.js
@@ -1,9 +1,24 @@
 import { Vue } from '@a-vue'
 
 Vue.directive('flying-context-trigger', {
-  inserted: function (el, bindings) {
+  bind: function (el, bindings) {
     if (bindings.value) {
+      el.$flyingContextObserver = new MutationObserver(() => {
+        if (!el.classList.contains('flyingContextTrigger')) {
+          el.classList.add('flyingContextTrigger')
+        }
+      })
+
+      el.$flyingContextObserver.observe(el, {
+        attributes: true,
+        attributeFilter: ['class']
+      })
+
       el.classList.add('flyingContextTrigger')
     }
+  },
+
+  unbind: function (el) {
+    el.$flyingContextObserver.disconnect()
   }
 })
