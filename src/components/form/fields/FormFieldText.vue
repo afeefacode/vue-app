474dedbd05185d3815fcafb76ac4c2849d248622
Kaken Bok <bok@kakenbok.de>
2022-07-13 12:17:41 +0200

    a-text-field may set 'null' to model insteadof zero or ''

--- src/components/form/fields/FormFieldText.vue
+++ src/components/form/fields/FormFieldText.vue
@@ -1,9 +1,11 @@
 <template>
   <a-text-field
-    v-model="model[name]"
+    :value="internalValue"
     :label="label || name"
     :validator="validator"
     v-bind="$attrs"
+    @input="textFieldValueChanged"
+    @blur="onBlur"
   />
 </template>
 
@@ -11,7 +13,70 @@
 import { Component, Mixins } from '@a-vue'
 import { FormFieldMixin } from '../FormFieldMixin'
 
-@Component
+@Component({
+  props: [{emptyNull: false}]
+})
 export default class FormFieldText extends Mixins(FormFieldMixin) {
+  internalValue = ''
+
+  created () {
+    this.setInternalValue(this.model[this.name])
+    this.$watch(() => this.model[this.name], value => {
+      this.setInternalValue(value)
+    })
+  }
+
+  onBlur () {
+    this.setInternalValue(this.model[this.name], true)
+  }
+
+  textFieldValueChanged (value) {
+    this.internalValue = value
+
+    // cast to number
+    if (this.isNumber) {
+      value = Number(value)
+      if (isNaN(value)) {
+        return // do not set anything to the model
+      }
+    }
+
+    // set model value to null if empty
+    if (this.emptyNull) {
+      if (this.isNumber) {
+        if (value === 0) {
+          value = null
+        }
+      } else {
+        if (!value) {
+          value = null
+        }
+      }
+    }
+
+    this.model[this.name] = value
+  }
+
+  setInternalValue (value, reset = false) {
+    if (this.isNumber) {
+      // reset text field if value is null but keep leading 0 (allows for copy and paste)
+      if (value === null) {
+        if (!reset && this.internalValue === '0') {
+          value = '0'
+        } else {
+          value = ''
+        }
+      }
+    } else { // null string should be ''
+      if (!value) {
+        value = ''
+      }
+    }
+    this.internalValue = value
+  }
+
+  get isNumber () {
+    return this.$attrs.number === ''
+  }
 }
 </script>
