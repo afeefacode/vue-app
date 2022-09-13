11f312463d4c0b9caf75762188f7c1eb3459b75a
makakken <post@makakken.de>
2022-09-13 11:53:58 +0200

    field-date-input which can be changed by typing

---
+++ src/components/ADateInput.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template #activator="{ on, attrs }">
+      <v-text-field
+        ref="input"
+        :value="formattedDate"
+        :label="label"
+        :style="cwm_widthStyle"
+        v-bind="{...$attrs, ...attrs}"
+        :rules="validationRules"
+        v-on="on"
+        @input="dateInputChanged"
+        @click:clear="clearDate"
+        @click.native="on.click"
+      />
+    </template>
+
+    <v-date-picker
+      v-if="menu"
+      :value="date"
+      no-title
+      :type="type"
+      v-bind="$attrs"
+      @input="dateChanged"
+    />
+  </v-menu>
+</template>
+
+
+<script>
+import { Component, Mixins, Watch } from '@a-vue'
+import { formatDate } from '@a-vue/utils/format-date'
+import { ComponentWidthMixin } from './mixins/ComponentWidthMixin'
+
+@Component({
+  props: ['value', 'validator', 'type']
+})
+export default class ADatePicker extends Mixins(ComponentWidthMixin) {
+  value_ = null
+  menu = false
+
+  created () {
+    if (this.value) {
+      this.value_ = this.value
+    }
+    this.$emit('input', this.value_)
+  }
+
+  mounted () {
+    if (this.validator) {
+      this.$refs.input.validate(true)
+    }
+  }
+
+  @Watch('value')
+  valueChanged () {
+    this.value_ = this.value
+  }
+
+  get date () {
+    return this.value_
+      ? this.value_.toISOString().substr(0, this.type === 'month' ? 7 : 10)
+      : null
+  }
+
+  get label () {
+    return this.$attrs.label
+  }
+
+  clearDate () {
+    this.value_ = null
+    this.$emit('input', this.value_)
+  }
+
+  dateInputChanged () {
+    const dateformat = /^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/
+    // Match the date format through regular expression
+    if (this.$refs.input.lazyValue.match(dateformat)) {
+      const [day, month, year] = this.$refs.input.lazyValue.split('.')
+      this.dateChanged(new Date(year + '-' + month + '-' + day))
+    } else {
+      console.log('invalid')
+      // invalidate form show error message
+    }
+  }
+
+  dateChanged (date) {
+    this.menu = false
+    this.value_ = new Date(date)
+    this.$emit('input', this.value_)
+  }
+
+  get formattedDate () {
+    const date = this.value_
+    if (!date) {
+      return null
+    }
+    if (this.type === 'month') {
+      const monthName = date.toLocaleString('default', { month: 'long' })
+      return monthName + ' ' + date.getFullYear()
+    }
+    return formatDate(new Date(date))
+  }
+
+  get validationRules () {
+    let rules = []
+    if (this.validator) {
+      rules = this.validator.getRules(this.label)
+    }
+    rules.push((v) => (!!v && v.match(/^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/)) || 'Kein gültiges Datum')
+    return rules
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+::v-deep .v-select__slot {
+  cursor: pointer;
+  input {
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/form/fields/FormFieldDateInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-date-input
+    v-model="model[name]"
+    :label="label || name"
+    :validator="validator"
+    v-bind="$attrs"
+  />
+</template>
+
+<script>
+import { Component, Mixins } from '@a-vue'
+import { FormFieldMixin } from '../FormFieldMixin'
+
+  @Component
+export default class FormFieldDateInput extends Mixins(FormFieldMixin) {
+  }
+</script>
+
