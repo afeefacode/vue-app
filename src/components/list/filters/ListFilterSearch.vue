d2f7cd29562e1ab62cd178dbb73f39930ce02000
Kaken Bok <bok@kakenbok.de>
2023-01-07 21:13:14 +0100

    support search field selection in list-filter-search

--- src/components/list/filters/ListFilterSearch.vue
+++ src/components/list/filters/ListFilterSearch.vue
@@ -1,16 +1,47 @@
 <template>
-  <a-text-field
-    ref="input"
-    v-model="filter.value"
-    :maxWidth="$attrs.maxWidth || maxWidth_"
-    :label="label || 'Suche'"
-    :debounce="500"
-    v-bind="$attrs"
-    clearable
-    hide-details
-    @keyup.esc="clearValue"
-    v-on="$listeners"
-  />
+  <div>
+    <a-text-field
+      ref="input"
+      v-model="filter.value"
+      :maxWidth="$attrs.maxWidth || maxWidth_"
+      :label="label_"
+      :debounce="500"
+      v-bind="$attrs"
+      clearable
+      hide-details
+      @keyup.esc="clearValue"
+      v-on="$listeners"
+    >
+      <template
+        v-if="hasQFieldOptions()"
+        #append
+      >
+        <a-context-menu>
+          <template #activator>
+            <!-- click.prevent brings hand cursor -->
+            <!-- mousedown.stop will prevent the input focused -->
+            <a-icon
+              @click.prevent
+              @mousedown.stop
+            >
+              {{ appendIcon }}
+            </a-icon>
+          </template>
+
+          <a-context-menu-item
+            v-for="option in qFieldOptions"
+            :key="option.title"
+            @click="filters.qfield.value = option.value"
+          >
+            <a-icon :class="{selected: filters.qfield.value === option.value}">
+              $checkIcon
+            </a-icon>
+            {{ option.title }}
+          </a-context-menu-item>
+        </a-context-menu>
+      </template>
+    </a-text-field>
+  </div>
 </template>
 
 
@@ -33,5 +64,39 @@ export default class ListFilterSearch extends Mixins(ListFilterMixin) {
   setFocus (force) {
     this.$refs.input.setFocus(force)
   }
+
+  get appendIcon () {
+    if (this.hasQFieldOptions()) {
+      return '$filterIcon'
+    }
+  }
+
+  hasQFieldOptions () {
+    return this.filters.qfield && this.filters.qfield.hasOptions()
+  }
+
+  get qFieldOptions () {
+    return this.filters.qfield.options
+  }
+
+  get label_ () {
+    const label = this.label || 'Suche'
+
+    if (this.hasQFieldOptions() && !this.filters.qfield.hasDefaultValueSet()) {
+      const selected = this.qFieldOptions.filter(o => o.value === this.filters.qfield.value)[0]
+      if (selected) {
+        return `${label} (${selected.title})`
+      }
+    }
+
+    return label
+  }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.contextMenuItem > *:not(.selected) {
+  opacity: 0;
+}
+</style>
