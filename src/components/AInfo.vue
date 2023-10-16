5089ae1e19831add0f01b6e4c6e6d8af2595644e
Kaken Bok <bok@kakenbok.de>
2023-10-16 21:24:11 +0200

    a-info xsmall style

--- src/components/AInfo.vue
+++ src/components/AInfo.vue
@@ -2,6 +2,7 @@
   <v-alert
     v-bind="$attrs"
     :style="{display: inline ? 'inline-block' : 'block'}"
+    :class="['a-info', {xSmall}]"
     dense
     border="left"
     colored-border
@@ -16,11 +17,20 @@
 import { Component, Vue } from '@a-vue'
 
 @Component({
-  props: [{inline: true}]
+  props: [{inline: true, xSmall: false}]
 })
 export default class AInfo extends Vue {
 }
 </script>
 
 <style lang="scss" scoped>
+.xSmall {
+  padding: .3rem .6rem;
+  font-size: .9rem;
+
+  :deep(.v-icon) {
+    margin-left: .2rem;
+    margin-right: .4rem;
+  }
+}
 </style>
