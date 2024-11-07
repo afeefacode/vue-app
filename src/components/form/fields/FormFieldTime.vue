2947aabcc61f4ec21ab2c1a47660bd805e1f8e33
Kaken Bok <bok@kakenbok.de>
2024-11-07 10:27:27 +0100

    #52 time picker nice way

--- src/components/form/fields/FormFieldTime.vue
+++ src/components/form/fields/FormFieldTime.vue
@@ -1,40 +1,12 @@
 <template>
-  <v-menu
-    ref="menu"
-    v-model="menu"
-    :close-on-content-click="false"
-    :nudge-right="40"
-    :return-value.sync="model[name]"
-    transition="scale-transition"
-    offset-y
-    max-width="290px"
-    min-width="290px"
-  >
-    <template #activator="{ on, attrs }">
-      <v-text-field
-        v-model="model[name]"
-        :label="label || name"
-        prepend-icon="mdi-clock-time-four-outline"
-        readonly
-        dense
-        outlined
-        v-bind="attrs"
-        v-on="on"
-      />
-    </template>
-    <v-time-picker
-      v-if="menu"
-      ref="timePicker"
-      v-model="model[name]"
-      :validator="validator"
-      v-bind="$attrs"
-      full-width
-      scrollable
-      format="24hr"
-      v-on="$listeners"
-      @click:minute="$refs.menu.save(model[name])"
-    />
-  </v-menu>
+  <a-time-picker
+    v-model="model[name]"
+    class="timePicker"
+    :label="label || name"
+    :validator="validator"
+    v-bind="$attrs"
+    v-on="$listeners"
+  />
 </template>
 
 <script>
@@ -43,9 +15,5 @@ import { FormFieldMixin } from '../FormFieldMixin'
 
 @Component
 export default class FormFieldTime extends Mixins(FormFieldMixin) {
-  menu = false
-  validate () {
-    this.$refs.timePicker.validate()
-  }
 }
 </script>
